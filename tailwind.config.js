02c1ed85c02db67fe8e5432a81ebd95f79be8278
hmacr <hmac.devo@gmail.com>
2023-09-07 14:08:29 +0530

    feat: style the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      'light-gray': 'hsl(212, 45%, 89%)',
+      'grayish-blue': 'hsl(220, 15%, 55%)',
+      'dark-blue': 'hsl(218. 44%, 22%)',
+    },
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
-  plugins: [],
 };
